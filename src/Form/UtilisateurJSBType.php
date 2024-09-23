f6b553217184db801d258441ca9bdfc57e603cf5
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-23 23:07:15 +0200

    Création du formulaire UtilisateurJSB

--- src/Form/UtilisateurJSBType.php
+++ src/Form/UtilisateurJSBType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\UtilisateurJSB;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +16,28 @@ class UtilisateurJSBType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('email')
-            ->add('motDePasse')
-            ->add('motDePasseConfirme')
+            ->add('nom', TextType::class, [
+                'label' => "Nom Complet"
+            ])
+            ->add('email', EmailType::class, [
+                'label' => "Votre Adresse mail"
+            ])
+            ->add('motDePasse', PasswordType::class, [
+                'label' => "Votre Mot de Passe"
+            ])
+            ->add('motDePasseConfirme', PasswordType::class, [
+                'label' => "Conformer Votre Mot de Passe"
+            ])
             ->add('createdAt', null, [
                 'widget' => 'single_text',
             ])
             ->add('updatedAt', null, [
                 'widget' => 'single_text',
             ])
+            ->add('enregistrer', SubmitType::class, [
+                'label' => "Enregistrer"
+            ])
+
         ;
     }
 
