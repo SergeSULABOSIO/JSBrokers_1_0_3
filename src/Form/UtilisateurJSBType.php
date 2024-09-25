f2e17dbb814244cb2649e203014a3764db469856
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-25 16:56:55 +0200

    Evenement lié au forumaire - PreSubmit et PreSetData - Tout fonctionne très bien

--- src/Form/UtilisateurJSBType.php
+++ src/Form/UtilisateurJSBType.php
@@ -4,12 +4,16 @@ namespace App\Form;
 
 use App\Entity\UtilisateurJSB;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PreSetDataEvent;
+use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class UtilisateurJSBType extends AbstractType
 {
@@ -28,10 +32,30 @@ class UtilisateurJSBType extends AbstractType
             ->add('motDePasseConfirme', PasswordType::class, [
                 'label' => "Conformer Votre Mot de Passe"
             ])
+            //Le bouton d'enregistrement / soumission
             ->add('enregistrer', SubmitType::class, [
                 'label' => "Enregistrer"
             ])
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->onPreSubmitActions(...))
+            ->addEventListener(FormEvents::PRE_SET_DATA, $this->onPreSetDataActions(...))
         ;
+    }
+
+    public function onPreSubmitActions(PreSubmitEvent $event)
+    {
+        // $data = $event->getData();
+        // $slugger = new AsciiSlugger();
+        // $data['nom'] = strtolower($slugger->slug($data['nom']));
+        // dd($data);
+    }
+
+    public function onPreSetDataActions(PreSetDataEvent $event)
+    {
+        // /** @var UtilisateurJSB */
+        // $user = $event->getData();
+        // $user->setNom("SULA BOSIO Serge");
+        // $user->setEmail("ssula@gmail.com");
+        // dd($user);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
