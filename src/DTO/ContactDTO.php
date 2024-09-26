b26dcf116b714d51de1769f81b7052d21da11fa1
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-26 16:22:17 +0200

    Formulaire de contact - envoie des mails

--- src/DTO/ContactDTO.php
+++ src/DTO/ContactDTO.php
@@ -2,8 +2,20 @@
 
 namespace App\DTO;
 
-class ContactDTO {
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ContactDTO
+{
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2)]
     public string $name = "";
+
+    #[Assert\NotBlank]
+    #[Assert\Email()]
     public string $email = "";
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 10, max:100)]
     public string $message = "";
 }
