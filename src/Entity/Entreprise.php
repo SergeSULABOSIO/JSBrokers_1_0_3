b94b5f8706e36ec280d3b4ad4e440b23e4e89280
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-28 00:25:45 +0200

    Creation des entreprises

---
+++ src/Entity/Entreprise.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\EntrepriseRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use App\Service\RefactoringJS\Evenements\Evenement;
+use App\Service\RefactoringJS\Evenements\Observateur;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Service\RefactoringJS\Commandes\ComDetecterEvenementAttribut;
+
+#[ORM\Entity(repositoryClass: EntrepriseRepository::class)]
+class Entreprise
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotBlank()]
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[Assert\NotBlank()]
+    #[ORM\Column(length: 255)]
+    private ?string $adresse = null;
+
+    #[Assert\NotBlank()]
+    #[ORM\Column(length: 255)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $rccm = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $idnat = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $numimpot = null;
+
+    #[Assert\NotBlank()]
+    private ?int $secteur = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function __construct()
+    {
+
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getRccm(): ?string
+    {
+        return $this->rccm;
+    }
+
+    public function setRccm(?string $rccm): self
+    {
+        $this->rccm = $rccm;
+
+        return $this;
+    }
+
+    public function getIdnat(): ?string
+    {
+        return $this->idnat;
+    }
+
+    public function setIdnat(?string $idnat): self
+    {
+        $this->idnat = $idnat;
+
+        return $this;
+    }
+
+    public function getNumimpot(): ?string
+    {
+        return $this->numimpot;
+    }
+
+    public function setNumimpot(?string $numimpot): self
+    {
+        $this->numimpot = $numimpot;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->nom;
+    }
+
+    public function getSecteur(): ?int
+    {
+        return $this->secteur;
+    }
+
+    public function setSecteur(?int $secteur): self
+    {
+        $this->secteur = $secteur;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
