5a3f499fe884726dfd70769520c6edf5fa4cfdcf
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-29 23:00:59 +0200

    Rubrique invité

--- src/Entity/Invite.php
+++ src/Entity/Invite.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\InviteRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: InviteRepository::class)]
 class Invite
@@ -13,6 +14,7 @@ class Invite
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank()]
     #[ORM\Column(length: 255)]
     private ?string $email = null;
 
