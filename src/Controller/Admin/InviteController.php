042585cdd0bbc9f904a85f5dcd1e104daa723785
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-29 21:13:09 +0200

    Rubrique invité

---
+++ src/Controller/Admin/InviteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class InviteController extends AbstractController
+{
+    public function index()
+    {
+        
+    }
+}
