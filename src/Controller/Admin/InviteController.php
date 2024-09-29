33e5c1f3fb49d52d4edf75c1e4466b48252c8a1d
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-29 21:28:49 +0200

    Rubrique invité

--- src/Controller/Admin/InviteController.php
+++ src/Controller/Admin/InviteController.php
@@ -2,27 +2,35 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
+#[Route("/admin/invite", name: 'admin.invite.')]
 class InviteController extends AbstractController
 {
+    #[Route(name: 'index')]
     public function index()
     {
         
     }
 
+
+    #[Route('/create', name: 'create')]
     public function create()
     {
         
     }
 
+
+    #[Route('/{id}', name: 'edit', requirements: ['id'=>Requirement::DIGITS])]
     public function edit()
     {
         
     }
 
+
+    #[Route('/{id}', name: 'remove', requirements: ['id'=>Requirement::DIGITS])]
     public function remove()
     {
         
