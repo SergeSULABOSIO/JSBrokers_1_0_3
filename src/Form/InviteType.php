f3daa13fe37f23471d82aacc97704f7bc1f41952
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-04 17:03:21 +0200

    Les relations

--- src/Form/InviteType.php
+++ src/Form/InviteType.php
@@ -2,13 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Entreprise;
 use App\Entity\Invite;
 use App\Services\FormListenerFactory;
 use DateTimeImmutable;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Event\PostSubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -18,10 +21,7 @@ class InviteType extends AbstractType
 
     public function __construct(
         private FormListenerFactory $ecouteurFormulaire
-    )
-    {
-        
-    }
+    ) {}
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -40,8 +40,10 @@ class InviteType extends AbstractType
             // ->add('updatedAt', null, [
             //     'widget' => 'single_text',
             // ])
-             //Le bouton d'enregistrement / soumission
-             ->add('enregistrer', SubmitType::class, [
+            ->add('entreprises')
+            
+            //Le bouton d'enregistrement / soumission
+            ->add('enregistrer', SubmitType::class, [
                 'label' => "Envoyer l'invitation"
             ])
             ->addEventListener(FormEvents::POST_SUBMIT, $this->ecouteurFormulaire->timeStamps())
