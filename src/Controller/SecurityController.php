c1977c7905c3dfc4695cf247c707863b651b5b29
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-12 11:18:19 +0200

    Sécurité - Registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use App\Entity\Utilisateur;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
