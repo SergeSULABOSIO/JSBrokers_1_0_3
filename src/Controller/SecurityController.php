b82fa45eddca2924b5278a2f0f32831402ba5e92
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-12 10:06:40 +0200

    Sécurité - Registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -12,21 +15,29 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            /** @var Utilisateur $user */
+            $user = $this->getUser();
+            if ($user->isVerified()) {
+                // dd($user);
+                // return new RedirectResponse($this->urlGenerator->generate("app_user_dashbord", ['idUtilisateur' => 32]));
+                return $this->redirectToRoute('app_user_dashbord', ['idUtilisateur' => 32]);
+            }
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', 
-        [
-            'last_username' => $lastUsername, 
-            'error' => $error,
-            'pageName' => "Authentification"
-        ]);
+        return $this->render(
+            'security/login.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error' => $error,
+                'pageName' => "Authentification"
+            ]
+        );
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
