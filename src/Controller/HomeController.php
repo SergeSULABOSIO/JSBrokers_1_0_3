e728e1b25f6f777c98d194ce9e662debb7635c02
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-12 11:16:44 +0200

    Sécurité - Registration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,26 +3,20 @@
 namespace App\Controller;
 
 use App\DTO\ContactDTO;
-use App\DTO\ConnexionDTO;
 use App\Form\ContactType;
 use App\Entity\Entreprise;
 use App\Entity\Utilisateur;
-use App\Form\ConnexionType;
 use App\Form\EntrepriseType;
-use App\Entity\UtilisateurJSB;
-use App\Form\UtilisateurJSBType;
 use Symfony\Component\Mime\Email;
 use App\Repository\InviteRepository;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\UtilisateurJSBRepository;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -42,98 +36,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
     {
-        //Création d'un utilisateur juste pour test
-        // $user = new Utilisateur();
-        // $user->setEmail("ssula@aib-brokers.com")
-        //     ->setNom("Serge SULA BOSIO de AIB")
-        //     ->setPassword($hasher->hashPassword($user, "abcd"))
-        //     ->setRoles([]);
-        // $em->persist($user);
-        // $em->flush();
-
         $this->addFlash("success", "Bienvenue chez JS Broker!");
         return $this->render('home/index.html.twig', [
             'pageName' => 'Home',
         ]);
     }
-
-
-
-
-    // #[Route('/user_login', name: 'app_user_login')]
-    // public function userLogin(Request $request): Response
-    // {
-    //     /** @var ConnexionDTO $data */
-    //     $data = new ConnexionDTO();
-    //     $form = $this->createForm(ConnexionType::class, $data);
-    //     $form->handleRequest($request);
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $user = null;
-    //         $tabResultats = $this->utilisateurJSBRepository->findBy([
-    //             'email' => $data->email,
-    //             'motDePasse' => $data->motdepasse
-    //         ]);
-    //         if (isset($tabResultats) && count($tabResultats) != 0) {
-    //             /** @var UtilisateurJSB $user */
-    //             $user = $tabResultats[0];
-    //             if ($user->getEmail() == $data->email && $user->getMotDePasse() == $data->motdepasse) {
-    //                 $this->addFlash("success", "Bienvenue " . $user->getNom());
-    //                 return $this->redirectToRoute('app_user_dashbord', [
-    //                     'idUtilisateur' => $user->getId(),
-    //                     'utilisateur' => $user,
-    //                 ]);
-    //             }
-    //         }
-    //         // dd($data, $tabResultats);
-    //         // dd($failed);
-    //         $this->addFlash("danger", "Identifiants incorrects.");
-    //         return $this->redirectToRoute('app_user_login', [
-    //             'pageName' => 'Connexion',
-    //             'form' => $form
-    //         ]);
-    //     }
-    //     return $this->render('home/user_login.html.twig', [
-    //         'pageName' => 'Connexion',
-    //         'form' => $form
-    //     ]);
-    // }
-
-
-
-
-    // #[Route('/user_registration/{idUtilisateur}', name: 'app_user_registration')]
-    // public function userRegistration(int $idUtilisateur, Request $request): Response
-    // {
-    //     $tittrePage = "Création du compte utilisateur";
-
-    //     /** @var UtilisateurJSB */
-    //     $utilisateurJSB = new UtilisateurJSB();
-    //     if ($idUtilisateur != -1) {
-    //         $utilisateurJSB = $this->utilisateurJSBRepository->find($idUtilisateur);
-    //         $tittrePage = "Edition du compte " . $utilisateurJSB->getNom();
-    //     }
-    //     $form = $this->createForm(UtilisateurJSBType::class, $utilisateurJSB);
-    //     $form->handleRequest($request);
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $this->manager->persist($utilisateurJSB);
-    //         $this->manager->flush();
-
-    //         if ($idUtilisateur != -1) {
-    //             $this->addFlash("success", "" . $utilisateurJSB->getNom() . ", votre profil est mis à jour avec succès.");
-    //             return $this->redirectToRoute("app_user_dashbord", [
-    //                 'idUtilisateur' => $utilisateurJSB->getId()
-    //             ]);
-    //         } else {
-    //             $this->addFlash("success", "" . $utilisateurJSB->getNom() . ", votre compte vient d'être créée.");
-    //             return $this->redirectToRoute("app_user_login");
-    //         }
-    //     }
-    //     return $this->render('home/user_registration.html.twig', [
-    //         'pageName' => $tittrePage,
-    //         'form' => $form,
-    //     ]);
-    // }
-
 
 
 
@@ -186,14 +93,6 @@ class HomeController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->manager->persist($entreprise);
-
-            // if ($form->get('thumbnailFile')->getData()) {
-            //     /** @var UploadedFile $photoProfile */
-            //     $photoProfile = $form->get('thumbnailFile')->getData();
-            //     $fileName = $entreprise->getId() . "." . $photoProfile->getClientOriginalExtension();
-            //     $photoProfile->move($this->getParameter("kernel.project_dir") . "/public/profile_entreprise", $fileName);
-            //     $entreprise->setThumbnail($fileName);
-            // }
 
             $this->manager->flush();
             if ($idEntreprise == -1) {
