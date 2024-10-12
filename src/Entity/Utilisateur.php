100bd71a28d37fe6c3d159f66dae3c7d3d98e6da
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-12 11:03:53 +0200

    Sécurité - Registration

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Cette adresse mail est déjà utilisée. Veuillez nous en fournir une autre.')]
 class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
