fbcc1f9695c18a5ba84e9ec3701f8e13001d56a3
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-15 12:10:21 +0200

    Doctrine

--- src/Form/InviteType.php
+++ src/Form/InviteType.php
@@ -2,30 +2,36 @@
 
 namespace App\Form;
 
-use App\Entity\Entreprise;
 use App\Entity\Invite;
-use App\Services\FormListenerFactory;
 use DateTimeImmutable;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bundle\SecurityBundle\Security;
+use App\Entity\Entreprise;
+use App\Entity\Utilisateur;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityRepository;
+use App\Services\FormListenerFactory;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Event\PostSubmitEvent;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class InviteType extends AbstractType
 {
 
     public function __construct(
-        private FormListenerFactory $ecouteurFormulaire
+        private FormListenerFactory $ecouteurFormulaire,
+        private Security $security
     ) {}
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+
         $builder
             ->add('email', EmailType::class, [
                 'label' => "L'addresse mail de l'invité",
@@ -46,7 +52,16 @@ class InviteType extends AbstractType
                 'choice_label' => "nom",
                 'multiple' => true,
                 'expanded' => true,
-                'by_reference' => false
+                'by_reference' => false,
+                //une requêt pour filtrer les elements de la liste d'options
+                'query_builder' => function (EntityRepository $er): QueryBuilder {
+                    /** @var Utilisateur $user */
+                    $user = $this->security->getUser();
+                    return $er->createQueryBuilder('e')
+                        ->where('e.utilisateur =:userId')
+                        ->setParameter('userId', $user->getId())
+                        ->orderBy('e.id', 'ASC');
+                },
             ])
 
             //Le bouton d'enregistrement / soumission
