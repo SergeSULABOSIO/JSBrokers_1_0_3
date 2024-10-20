b837a2765ddc64aa3dc46684cec16b9dafd9d6ad
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-21 01:33:38 +0200

    Entreprise - Monnaie

--- src/Controller/Admin/MonnaieController.php
+++ src/Controller/Admin/MonnaieController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Entreprise;
 use App\Entity\Invite;
+use App\Entity\Monnaie;
 use App\Form\InviteType;
 use App\Entity\Utilisateur;
 use App\Event\InvitationEvent;
+use App\Form\MonnaieType;
 use App\Repository\InviteRepository;
 use App\Repository\EntrepriseRepository;
 use App\Repository\MonnaieRepository;
@@ -37,12 +38,9 @@ class MonnaieController extends AbstractController
     {
         $page = $request->query->getInt("page", 1);
 
-        /** @var Utilisateur $user */
-        $user = $this->getUser();
-
         return $this->render('admin/monnaie/index.html.twig', [
             'pageName' => "Monnaies",
-            'utilisateur' => $user,
+            'utilisateur' => $this->getUser(),
             'entreprise' => $this->entrepriseRepository->find($idEntreprise),
             'monnaies' => $this->monnaieRepository->paginateMonnaie($idEntreprise, $page),
             'page' => $page = $request->query->getInt("page", 1),
@@ -50,85 +48,75 @@ class MonnaieController extends AbstractController
     }
 
 
-    #[Route('/create', name: 'create')]
-    public function create(Request $request, EventDispatcherInterface $dispatcher)
+    #[Route('/{idEntreprise}', name: 'create')]
+    public function create($idEntreprise, Request $request)
     {
-        // dd($this->entrepriseRepository->getNBMyProperEntreprises());
         /** @var Utilisateur $user */
         $user = $this->getUser();
 
-        /** @var Invite */
-        $invite = new Invite();
-        $form = $this->createForm(InviteType::class, $invite);
+        /** @var Monnaie */
+        $monnaie = new Monnaie();
+        $form = $this->createForm(MonnaieType::class, $monnaie);
         $form->handleRequest($request);
 
-        if ($this->entrepriseRepository->getNBMyProperEntreprises() != 0) {
-            if ($form->isSubmitted() && $form->isValid()) {
-                $this->manager->persist($invite);
-                $this->manager->flush();
-                try {
-                    //Envoie de l'email de notification
-                    //Lancer un évènement
-                    $dispatcher->dispatch(new InvitationEvent($invite));
-                    $this->addFlash("success", $invite->getEmail() . " a été invité avec succès.");
-                } catch (\Throwable $th) {
-                    //throw $th;
-                    $this->addFlash("danger", "Echec d'envoie de l'email d'invitation.");
-                }
-                return $this->redirectToRoute("admin.invite.index");
-            }
-        } else {
-            $this->addFlash("danger", "Désolé " . $user->getNom() . ", vous n'avez pas le droit d'inviter des utilisateurs. Vous ne pouvez inviter d'utilisateurs que pour une ou des entreprises que vous avez créées vous-mêmes, non pas pour une ou des entreprises où vous êtes invités.");
-            return $this->redirectToRoute("admin.invite.index");
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($monnaie);
+            $this->manager->flush();
+            return $this->redirectToRoute("admin.monnaie.index", [
+                'idEntreprise' => $idEntreprise,
+            ]);
         }
-        return $this->render('admin/invite/create.html.twig', [
+        return $this->render('admin/monnaie/create.html.twig', [
             'pageName' => 'Nouveau',
-            'utilisateur' => $user,
-            'nbEntreprises' => $this->entrepriseRepository->getNBEntreprises(),
-            'nbInvites' => $this->inviteRepository->getNBInvites(),
+            'utilisateur' => $this->getUser(),
+            'entreprise' => $this->entrepriseRepository->find($idEntreprise),
             'form' => $form,
         ]);
     }
 
 
-    #[Route('/{id}', name: 'edit', requirements: ['id' => Requirement::DIGITS], methods: ['GET', 'POST'])]
-    public function edit(Invite $invite, Request $request)
+    #[Route('/{idEntreprise}/{idMonnaie}', name: 'edit', requirements: ['idEntreprise' => Requirement::DIGITS], methods: ['GET', 'POST'])]
+    public function edit($idEntreprise, $idMonnaie, Request $request)
     {
         // dd($invite);
         /** @var Utilisateur $user */
         $user = $this->getUser();
 
-        $form = $this->createForm(InviteType::class, $invite);
+        /** @var Monnaie */
+        $monnaie = $this->monnaieRepository->find($idMonnaie);
+
+        $form = $this->createForm(InviteType::class, $monnaie);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->manager->persist($invite); //On peut ignorer cette instruction car la fonction flush suffit.
+            $this->manager->persist($monnaie); //On peut ignorer cette instruction car la fonction flush suffit.
             $this->manager->flush();
-            $this->addFlash("success", $invite->getEmail() . " a été modifié avec succès.");
-            return $this->redirectToRoute("admin.invite.index");
+            $this->addFlash("success", $monnaie->getCode() . " a été modifiée avec succès.");
+            return $this->redirectToRoute("admin.monnaie.index");
         }
-        return $this->render('admin/invite/edit.html.twig', [
+        return $this->render('admin/monnaie/edit.html.twig', [
             'pageName' => "Edition",
             'utilisateur' => $user,
-            'invite' => $invite,
-            'nbEntreprises' => $this->entrepriseRepository->getNBEntreprises(),
-            'nbInvites' => $this->inviteRepository->getNBInvites(),
+            'monnaie' => $monnaie,
+            'entreprise' => $this->entrepriseRepository->find($idEntreprise),
             'form' => $form,
         ]);
     }
 
 
-    #[Route('/{id}', name: 'remove', requirements: ['id' => Requirement::DIGITS], methods: ['DELETE'])]
-    public function remove(Request $request, Invite $invite)
+    #[Route('/{idEntreprise}/{idMonnaie}', name: 'remove', requirements: ['idMonnaie' => Requirement::DIGITS], methods: ['DELETE'])]
+    public function remove($idEntreprise, $idMonnaie, Request $request)
     {
-        $inviteId = $invite->getId();
-        $message = $invite->getEmail() . " a été supprimé avec succès.";
-        $this->manager->remove($invite);
+        /** @var Monnaie */
+        $monnaie = $this->monnaieRepository->find($idMonnaie);
+        $message = $monnaie->getNom() . " a été supprimé avec succès.";
+        $this->manager->remove($monnaie);
         $this->manager->flush();
 
-        if($request->getPreferredFormat() == TurboBundle::STREAM_FORMAT){
+        if ($request->getPreferredFormat() == TurboBundle::STREAM_FORMAT) {
             $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
             return $this->render("admin/invite/delete.html.twig", [
-                'inviteId' => $inviteId,
+                'monnaieId' => $monnaie,
                 'messages' => $message,
                 'type' => "success",
             ]);
