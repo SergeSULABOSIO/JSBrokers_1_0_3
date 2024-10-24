724c24aacf205e92e5a80dd6e7faaa101807a05d
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-24 22:28:57 +0200

    Entreprise - Collection des monnaies

--- assets/controllers/form-collection-monnaies_controller.js
+++ assets/controllers/form-collection-monnaies_controller.js
@@ -34,7 +34,7 @@ export default class extends Controller {
     }
 
     /**
-     * @param {MouseEvent} e 
+     * @param {MouseEvent} e
      */
     addElement = (e) => {
         e.preventDefault();
