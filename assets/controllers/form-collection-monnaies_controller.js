e3fb22fb294c275f04d5b539599f5297c9cee2ab
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-24 14:19:59 +0200

    Entreprise - Collection des monnaies

--- assets/controllers/form-collection-monnaies_controller.js
+++ assets/controllers/form-collection-monnaies_controller.js
@@ -2,17 +2,17 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
-        const btn = document.createElement("button");
-        btn.setAttribute('class', "btn btn-secondary");
-        btn.setAttribute('type', "button");
-        btn.innerText = "Ajouter un élement";
-        btn.addEventListener('click', this.addElement);
-        this.element.append(btn);
+        const btn = document.createElement("button")
+        btn.setAttribute('class', "btn btn-secondary")
+        btn.setAttribute('type', "button")
+        btn.innerText = "Ajouter un élement"
+        btn.addEventListener('click', this.addElement)
+        this.element.append(btn)
         // console.log('hello, je suis le form collection controller stumilus!')
     }
 
 
     addElement = (e) => {
-        e.preventDefault();
+        e.preventDefault()
     }
 }
