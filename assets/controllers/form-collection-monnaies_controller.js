6c5408b338a4a576d07794697346162d1f950b1a
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-25 15:10:56 +0200

    Entreprise - Collection des monnaies

--- assets/controllers/form-collection-monnaies_controller.js
+++ assets/controllers/form-collection-monnaies_controller.js
@@ -10,98 +10,35 @@ export default class extends Controller {
     connect() {
         this.index = this.element.childElementCount;
         // console.log("Nombre d'elements = " + this.index);
-
         const btnAjouter = document.createElement("button");
         btnAjouter.setAttribute('class', "btn btn-secondary");
         btnAjouter.setAttribute('type', "button");
         btnAjouter.innerText = "Ajouter un élement";
         btnAjouter.addEventListener('click', this.addElement);
         
-        this.element.childNodes.forEach(this.addDeleteButton);
-
-        //Accordeon
-        // const accordion = document.createElement("div");
-        // accordion.setAttribute('class', "accordion");
-        // accordion.setAttribute('id', "accordionExample");
-        // const i = 1;
-        // this.element.childNodes.forEach(elementCollection => {
-        //     // this.addAccordion(i, accordion, elementCollection);
-        //     i++;
-        // });
-        // this.element.append(accordion);
-
-
-
-        // this.element.childNodes.forEach(this.addAccordion);
-        // this.element.childNodes.forEach(this.addDeleteButton);
+        //Pour chaque element de la collection
+        this.element.childNodes.forEach(elementDeLaCollection => {
+            elementDeLaCollection.setAttribute('class', "border rounded border-secondary p-3 mb-2");
+            this.addDeleteButton(elementDeLaCollection);
+        });
         this.element.append(btnAjouter);
-
-        // this.element.innerText = "ECRASE!!!!!!!";
     }
+
+    
 
     /**
      * @param {HTMLElement} elementDeLaCollection 
      */
-    addAccordion = (i, accordion, elementDeLaCollection) => {
-        // elementDeLaCollection.setAttribute('class', "border rounded border-secondary p-3 mb-2");
-        // this.addDeleteButton(elementDeLaCollection);
-        
-        // const data = elementDeLaCollection.innerHTML;
-        // elementDeLaCollection.innerHTML = "";
-        
-        //Accordeon-item
-        const accordionItem = document.createElement("div");
-        accordionItem.setAttribute('class', "accordion-item");
-        //Accordeon-header
-        const accordionHeader = document.createElement("h2");
-        accordionHeader.setAttribute('class', "accordion-header");
-        //Accordeon-button
-        const accordionButton = document.createElement("button");
-        accordionButton.setAttribute('class', "accordion-button collapsed");
-        accordionButton.setAttribute('type', "button");
-        accordionButton.setAttribute('data-bs-toggle', "collapse");
-        accordionButton.setAttribute('data-bs-target', "#collapseTwo");
-        accordionButton.setAttribute('aria-expanded', "false");
-        accordionButton.setAttribute('aria-controls', "collapseTwo");
-        accordionButton.innerText = "Accordion Item #1";
-        //Accordeon-collapse
-        const accordionCollapse = document.createElement("div");
-        accordionCollapse.setAttribute('id', "collapseTwo");
-        accordionCollapse.setAttribute('class', "accordion-collapse collapse");
-        accordionCollapse.setAttribute('data-bs-parent', "#accordionExample");
-        //Accordeon-body
-        const accordionBody = document.createElement("div");
-        accordionBody.setAttribute('class', "accordion-body");
-        accordionBody.innerHTML = "<strong>Voici un petit exemple.</strong> Juste pour montrer que ça marche.";
-        // accordionBody.innerHTML = data;
-
-        //Construction
-        accordionCollapse.append(accordionBody);
-        accordionHeader.append(accordionButton);
-        accordionItem.append(accordionHeader);
-        accordionItem.append(accordionCollapse);
-        accordion.append(accordionItem);
-
-        // this.element.append(accordion);
-        
-        // elementDeLaCollection.innerHTML = data;
-        // elementDeLaCollection.append(accordion);
-    }
-
-
-    /**
-     * @param {HTMLElement} item 
-     */
-    addDeleteButton = (item) => {
+    addDeleteButton = (elementDeLaCollection) => {
         const btnSupprimer = document.createElement("button");
         btnSupprimer.setAttribute('class', "btn btn-danger mt-2");
         btnSupprimer.setAttribute('type', "button");
         btnSupprimer.innerText = "Supprimer";
         btnSupprimer.addEventListener('click', e => {
             e.preventDefault();
-            item.remove();
+            elementDeLaCollection.remove();
         });
-        item.append(btnSupprimer);
+        elementDeLaCollection.append(btnSupprimer);
     }
 
     /**
