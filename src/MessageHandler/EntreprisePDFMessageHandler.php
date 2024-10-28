d96fa305b380f07785694eca35eee7fd72edf76e
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-10-28 11:09:30 +0100

    Entreprise - Messenger

--- src/MessageHandler/EntreprisePDFMessageHandler.php
+++ src/MessageHandler/EntreprisePDFMessageHandler.php
@@ -24,23 +24,24 @@ final class EntreprisePDFMessageHandler
 
     public function __invoke(EntreprisePDFMessage $message): void
     {
-        //Générer du code php avec Gotenberg
-        $process = new Process([
-            'curl',
-            '--request',
-            'POST',
-            sprintf('%s/forms/chromium/convert/url', $this->gotenbegEndpoint),
-            // $this->gotenbegEndpoint . '/forms/chromium/convert/url',
-            '--form',
-            sprintf('url=%s', $this->urlGenerator->generate('admin.entreprise.pdf', ['id' => $message->id], UrlGeneratorInterface::ABSOLUTE_URL)),
-            // 'url=' . $this->urlGenerator->generate('admin.entreprise.pdf', ['id' => $message->id], UrlGeneratorInterface::ABSOLUTE_URL),
-            '-o',
-            $this->path . '/' . $message->id . ".pdf"
-        ]);
-        $process->run();
-        if(!$process->isSuccessful()){
-            throw new ProcessFailedException($process);
-        }
+        //Générer du code php avec Gotenberg : j'ai des difficultés pour installer Gotenberg, je reviendrais ici plus tard.
+        // $process = new Process([
+        //     'curl',
+        //     '--request',
+        //     'POST',
+        //     sprintf('%s/forms/chromium/convert/url', $this->gotenbegEndpoint),
+        //     // $this->gotenbegEndpoint . '/forms/chromium/convert/url',
+        //     '--form',
+        //     sprintf('url=%s', $this->urlGenerator->generate('admin.entreprise.pdf', ['id' => $message->id], UrlGeneratorInterface::ABSOLUTE_URL)),
+        //     // 'url=' . $this->urlGenerator->generate('admin.entreprise.pdf', ['id' => $message->id], UrlGeneratorInterface::ABSOLUTE_URL),
+        //     '-o',
+        //     $this->path . '/' . $message->id . ".pdf"
+        // ]);
+        // $process->run();
+        // if(!$process->isSuccessful()){
+        //     throw new ProcessFailedException($process);
+        // }
         // file_put_contents($this->path . '/' . $message->id . ".pdf", ""); //Généer du pdf avec du code php simplement
+        file_put_contents($this->path . '/' . $message->id . ".txt", "Ceci est juste un exemple"); //Généer du pdf avec du code php simplement
     }
 }
