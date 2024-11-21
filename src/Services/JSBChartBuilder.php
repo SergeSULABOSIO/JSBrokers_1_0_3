841061d4a42f214b5d7f2b421afda4b71c909b0a
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-11-21 10:08:12 +0100

    Entreprise - Tableau de bord

---
+++ src/Services/JSBChartBuilder.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\UX\Chartjs\Model\Chart;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+
+
+class JSBChartBuilder
+{
+
+    public function __construct(
+        private Security $security,
+        private ChartBuilderInterface $chartBuilder,
+    )
+    {
+        
+    }
+
+    public function newChartPerMonth()
+    {
+        //Construction de l'histogramme
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData([
+            'labels' => [
+                'January', 
+                'February', 
+                'March', 
+                'April', 
+                'May', 
+                'June', 
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December',
+            ],
+            'datasets' => [
+                [
+                    'label' => 'Revenue',
+                    'backgroundColor' => "gray",//'rgb(255, 99, 132)',
+                    'borderColor' => 'white', //'rgb(255, 99, 132)',
+                    'data' => [0, 10, 50, 2, 20, 90, 45, 25, 15, 60, 5, 90],
+                ],
+            ],
+        ]);
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+        return $chart;
+    }
+
+    public function newChartPerInsurer()
+    {
+        //Construction de l'histogramme
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_PIE);
+        $chart->setData([
+            'labels' => [
+                'SFA Congo SA (10%)', 
+                'SUNU ASSURANCE IARD (50%)', 
+                'ACTIVA  (20%)', 
+                'RAWSUR', 
+                'MAYFAIRE'
+            ],
+            'datasets' => [
+                [
+                    'label' => 'Insurers',
+                    'backgroundColor' => [
+                        'Red',
+                        'Blue',
+                        'Green',
+                        'Grey',
+                        'Orange'
+                    ],//'rgb(255, 99, 132)',
+                    'borderColor' => 'white', //'rgb(255, 99, 132)',
+                    'data' => [1, 5, 2, 2, 9],
+                    'hoverOffset' => 30,
+                ],
+            ],
+        ]);
+
+        return $chart;
+    }
+
+    public function newChartPerRenewalStatus()
+    {
+        //Construction de l'histogramme
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_PIE);
+        $chart->setData([
+            'labels' => [
+                'LOST', 
+                'ONCE-OFF', 
+                'RENEWED', 
+                'EXTENDED', 
+                'RUNNING'
+            ],
+            'datasets' => [
+                [
+                    'label' => 'Renewal Status',
+                    'backgroundColor' => [
+                        'Red',
+                        'Blue',
+                        'Green',
+                        'Grey',
+                        'Orange'
+                    ],//'rgb(255, 99, 132)',
+                    'borderColor' => 'white', //'rgb(255, 99, 132)',
+                    'data' => [1, 5, 2, 2, 9],
+                    'hoverOffset' => 30,
+                ],
+            ],
+        ]);
+
+        return $chart;
+    }
+
+    public function newChartPerPartner()
+    {
+        //Construction de l'histogramme
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_PIE);
+        $chart->setData([
+            'labels' => [
+                'OURSELVES', 
+                'OLEA', 
+                'MARSH', 
+                'MONT-BLANC', 
+                'AFINBRO',
+                'AGL',
+                "O'NEILS",
+                "MERCER",
+            ],
+            'datasets' => [
+                [
+                    'label' => 'Broker Partners',
+                    'backgroundColor' => [
+                        'Red',
+                        'Blue',
+                        'Green',
+                        'Grey',
+                        'Orange',
+                        'Pink',
+                        'Magenta',
+                        'Yellow',
+                    ],//'rgb(255, 99, 132)',
+                    'borderColor' => 'white', //'rgb(255, 99, 132)',
+                    'data' => [1, 5, 2, 2, 9, 5, 8, 2],
+                    'hoverOffset' => 30,
+                ],
+            ],
+        ]);
+
+        return $chart;
+    }
+
+    public function newChartPerRisk()
+    {
+        //Construction de l'histogramme
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_PIE);
+        $chart->setData([
+            'labels' => [
+                'FAP', 
+                'PVT', 
+                'PDBI', 
+                'MOTOR TPL', 
+                'CIT',
+                'GIT',
+                "GPA",
+                "MEDICAL",
+            ],
+            'datasets' => [
+                [
+                    'label' => 'Line Of Business',
+                    'backgroundColor' => [
+                        'Red',
+                        'Blue',
+                        'Green',
+                        'Grey',
+                        'Orange',
+                        'Pink',
+                        'Magenta',
+                        'Yellow',
+                    ],//'rgb(255, 99, 132)',
+                    'borderColor' => 'white', //'rgb(255, 99, 132)',
+                    'data' => [1, 5, 2, 2, 9, 5, 8, 2],
+                    'hoverOffset' => 30,
+                ],
+            ],
+        ]);
+
+        return $chart;
+    }
+}
