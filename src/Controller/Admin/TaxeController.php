03cc827d1553b3a9c14f34abb5c832925183c1b9
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-12-10 16:57:48 +0100

    Fonctions utilitaires...Taxes

---
+++ src/Controller/Admin/TaxeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Constantes\Constante;
+use App\Constantes\MenuActivator;
+use App\Repository\TaxeRepository;
+use App\Repository\InviteRepository;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+#[Route("/admin/taxe", name: 'admin.taxe.')]
+#[IsGranted('ROLE_USER')]
+class TaxeController extends AbstractController
+{
+    public MenuActivator $activator;
+
+    public function __construct(
+        private MailerInterface $mailer,
+        private TranslatorInterface $translator,
+        private EntityManagerInterface $manager,
+        private EntrepriseRepository $entrepriseRepository,
+        private InviteRepository $inviteRepository,
+        private TaxeRepository $taxeRepository,
+        private Constante $constante,
+    ) {
+        $this->activator = new MenuActivator(MenuActivator::GROUPE_FINANCE);
+    }
+
+
+    #[Route('/index/{idEntreprise}', name: 'index', requirements: ['idEntreprise' => Requirement::DIGITS], methods: ['GET', 'POST'])]
+    public function index($idEntreprise, Request $request)
+    {
+        $page = $request->query->getInt("page", 1);
+
+        return $this->render('admin/taxe/index.html.twig', [
+            'pageName' => "Taxes",
+            'utilisateur' => $this->getUser(),
+            'entreprise' => $this->entrepriseRepository->find($idEntreprise),
+            'taxes' => $this->taxeRepository->paginateTaxe($idEntreprise, $page),
+            'page' => $page,
+            'constante' => $this->constante,
+            'activator' => $this->activator,
+        ]);
+    }
+}
--- src/Repository/TaxeRepository.php
+++ src/Repository/TaxeRepository.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace App\Repository;
 
 
 use App\Entity\Taxe;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Taxe>
@@ -16,8 +20,11 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class TaxeRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
+    public function __construct(
+        ManagerRegistry $registry,
+        private PaginatorInterface $paginator,
+        private Security $security
+    ) {
         parent::__construct($registry, Taxe::class);
     }
 
@@ -45,7 +52,7 @@ class TaxeRepository extends ServiceEntityRepository
     public function findByMotCle($criteres): array
     {
         $query = $this->createQueryBuilder('t')
-            ->where('t.nom like :valMotCle')
+            ->where('t.code like :valMotCle')
             ->orWhere('t.description like :valMotCle')
             ->orWhere('t.organisation like :valMotCle')
             ->setParameter('valMotCle', '%' . $criteres['motcle'] . '%')
@@ -60,15 +67,33 @@ class TaxeRepository extends ServiceEntityRepository
 
 
     public function stat_get_nombres_enregistrements()
-       {
-           return $this->createQueryBuilder('a')
-               ->select('count(a.id) as nombre')
+    {
+        return $this->createQueryBuilder('a')
+            ->select('count(a.id) as nombre')
             //    ->select('a.exampleField = :val')
             //    ->setParameter('val', $value)
-               ->getQuery()
-               ->getSingleScalarResult()
-           ;
-       }
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+    }
+
+    public function paginateTaxe(int $idEntreprise, int $page): PaginationInterface
+    {
+        /** @var Utilisateur $user */
+        $user = $this->security->getUser();
+
+        return $this->paginator->paginate(
+            $this->createQueryBuilder("m")
+                // ->leftJoin("e.invites", "i")
+                ->where('m.entreprise = :entrepriseId')
+                // ->orWhere("i.email = :userEmail")
+                ->setParameter('entrepriseId', '' . $idEntreprise . '')
+                // ->setParameter('userEmail', '' . $user->getEmail() . '')
+                ->orderBy('m.id', 'DESC'),
+            $page,
+            20,
+        );
+    }
 
     //    public function findOneBySomeField($value): ?Taxe
     //    {
