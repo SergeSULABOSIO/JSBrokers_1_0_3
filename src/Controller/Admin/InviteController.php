de65d63ec650a8a50920d1e76c3f505ef31caa37
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-12-10 11:20:34 +0100

    Fonctions utilitaires...

--- src/Controller/Admin/InviteController.php
+++ src/Controller/Admin/InviteController.php
@@ -133,14 +133,14 @@ class InviteController extends AbstractController
         $this->manager->remove($invite);
         $this->manager->flush();
 
-        if ($request->getPreferredFormat() == TurboBundle::STREAM_FORMAT) {
-            $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
-            return $this->render("admin/invite/delete.html.twig", [
-                'inviteId' => $inviteId,
-                'messages' => $message,
-                'type' => "success",
-            ]);
-        }
+        // if ($request->getPreferredFormat() == TurboBundle::STREAM_FORMAT) {
+        //     $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+        //     return $this->render("admin/invite/delete.html.twig", [
+        //         'inviteId' => $inviteId,
+        //         'messages' => $message,
+        //         'type' => "success",
+        //     ]);
+        // }
         $this->addFlash("success", $message);
         return $this->redirectToRoute("admin.invite.index");
     }
