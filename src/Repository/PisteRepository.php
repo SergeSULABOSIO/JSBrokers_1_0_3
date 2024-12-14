4c4dab0bdc8b9b76c088ac99094f3822164291d1
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-12-14 03:06:30 +0100

    Fonctions utilitaires...Contact

--- src/Repository/PisteRepository.php
+++ src/Repository/PisteRepository.php
@@ -3,15 +3,22 @@
 namespace App\Repository;
 
 use App\Entity\Piste;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Piste>
  */
 class PisteRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(
+        ManagerRegistry $registry,
+        private PaginatorInterface $paginator,
+        private Security $security
+    )
     {
         parent::__construct($registry, Piste::class);
     }
@@ -40,4 +47,21 @@ class PisteRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
+    public function paginate(int $idEntreprise, int $page): PaginationInterface
+    {
+        /** @var Utilisateur $user */
+        // $user = $this->security->getUser();
+
+        return $this->paginator->paginate(
+            $this->createQueryBuilder("m")
+                // ->leftJoin("e.invites", "i")
+                ->where('m.entreprise = :entrepriseId')
+                // ->orWhere("i.email = :userEmail")
+                ->setParameter('entrepriseId', '' . $idEntreprise . '')
+                // ->setParameter('userEmail', '' . $user->getEmail() . '')
+                ->orderBy('m.id', 'DESC'),
+            $page,
+            20,
+        );
+    }
 }
