c4936a3e33d9b1431630c76a82b08c1bc67ef5c9
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-12-20 15:02:47 +0100

    Fonctions utilitaires...Entreprise

--- assets/controllers/form-collection-entites_controller.js
+++ assets/controllers/form-collection-entites_controller.js
@@ -9,17 +9,23 @@ export default class extends Controller {
 
     connect() {
         this.index = this.element.childElementCount;
-        // console.log("Nombre d'elements = " + this.index);
+        console.log("Nombre d'elements = " + this.index);
+
+        //Construction de l'élement Bouton pour la supression
         const btnAjouter = document.createElement("button");
         btnAjouter.setAttribute('class', "btn btn-secondary");
         btnAjouter.setAttribute('type', "button");
         btnAjouter.innerHTML = this.addLabelValue || "Add";
         btnAjouter.addEventListener('click', this.addElement);
         
-        //Pour chaque element de la collection
+        //On ajoute le bouton SUPPRIMER sur chaque element de la collection
         this.element.childNodes.forEach(elementDeLaCollection => {
             elementDeLaCollection.setAttribute('class', "border shadow rounded border-secondary p-3 mb-2 bg-white");
             this.addDeleteButton(elementDeLaCollection);
+
+            //Analyses
+            console.log("fieldSet class = " + elementDeLaCollection.getAttribute("type"));
+            
         });
         this.element.append(btnAjouter);
         this.element.setAttribute('class', "border border-0 rounded p-3 mb-2 bg-light");
@@ -47,7 +53,7 @@ export default class extends Controller {
      */
     addElement = (e) => {
         e.preventDefault();
-        // console.log('On vient de cliquer sur le bouton "Ajouter un élement".' + e);
+        // console.log('On vient de cliquer sur le bouton "Ajouter un élement".' + e + ". Son id est = " + this.element.getAttribute("id"));
         const elementPrototype = document.createRange().createContextualFragment(
             this.element.dataset['prototype'].replaceAll('__name__', this.index)
         ).firstElementChild;
