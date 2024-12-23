28e430b953893c02e977799cfd3116889c8c2f03
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-12-23 09:03:00 +0100

    Fonctions utilitaires...Entreprise

--- src/Form/TacheType.php
+++ src/Form/TacheType.php
@@ -17,12 +17,15 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TacheType extends AbstractType
 {
 
     public function __construct(
         private FormListenerFactory $ecouteurFormulaire,
+        private TranslatorInterface $translatorInterface,
         private Security $security
     ) {}
     
@@ -55,25 +58,44 @@ class TacheType extends AbstractType
                     "Pas encore." => false,
                 ],
             ])
-            ->add('invite', EntityType::class, [
-                'label' => "Invité",
-                'class' => Invite::class,
-                'choice_label' => 'id',
-            ])
+            // ->add('invite', EntityType::class, [
+            //     'label' => "Invité",
+            //     'class' => Invite::class,
+            //     'choice_label' => 'id',
+            // ])
             ->add('executor', EntityType::class, [
                 'label' => "Executeur",
                 'class' => Invite::class,
                 'choice_label' => 'id',
             ])
-            ->add('piste', EntityType::class, [
-                'label' => "Piste",
-                'class' => Piste::class,
-                'choice_label' => 'id',
-            ])
-            ->add('cotation', EntityType::class, [
-                'label' => "Cotation",
-                'class' => Cotation::class,
-                'choice_label' => 'id',
+            // ->add('piste', EntityType::class, [
+            //     'label' => "Piste",
+            //     'class' => Piste::class,
+            //     'choice_label' => 'id',
+            // ])
+            // ->add('cotation', EntityType::class, [
+            //     'label' => "Cotation",
+            //     'class' => Cotation::class,
+            //     'choice_label' => 'id',
+            // ])
+            ->add('feedbacks', CollectionType::class, [
+                'label' => "tache_form_label_feedback",
+                'entry_type' => FeedbackType::class,
+                'by_reference' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'entry_options' => [
+                    'label' => false,
+                ],
+                'attr' => [
+                    'data-controller' => 'form-collection-entites',
+                    'data-form-collection-entites-add-label-value' => $this->translatorInterface->trans("commom_add"),
+                    'data-form-collection-entites-delete-label-value' => $this->translatorInterface->trans("commom_delete"),
+                    'data-form-collection-entites-edit-label-value' => $this->translatorInterface->trans("commom_edit"),
+                    'data-form-collection-entites-close-label-value' => $this->translatorInterface->trans("commom_close"),
+                    'data-form-collection-entites-new-element-label-value' => $this->translatorInterface->trans("commom_new_element"),
+                    'data-form-collection-entites-view-field-value' => "description",
+                ],
             ])
             //Le bouton d'enregistrement / soumission
             ->add('enregistrer', SubmitType::class, [
