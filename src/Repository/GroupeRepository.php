a79a8e54d6bb71473e061ff579320b3e2f45255d
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-01-04 14:16:53 +0100

    Relations entre entites

--- src/Repository/GroupeRepository.php
+++ src/Repository/GroupeRepository.php
@@ -3,15 +3,22 @@
 namespace App\Repository;
 
 use App\Entity\Groupe;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Groupe>
  */
 class GroupeRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(
+        ManagerRegistry $registry,
+        private PaginatorInterface $paginator,
+        private Security $security
+    )
     {
         parent::__construct($registry, Groupe::class);
     }
@@ -40,4 +47,15 @@ class GroupeRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
+    public function paginateForEntreprise(int $idEntreprise, int $page): PaginationInterface
+    {
+        return $this->paginator->paginate(
+            $this->createQueryBuilder("groupe")
+                ->where('groupe.entreprise = :entrepriseId')
+                ->setParameter('entrepriseId', '' . $idEntreprise . '')
+                ->orderBy('groupe.id', 'DESC'),
+            $page,
+            20,
+        );
+    }
 }
