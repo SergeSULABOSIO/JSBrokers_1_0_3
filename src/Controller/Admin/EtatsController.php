da17e0d9b807e958ecd616b863307c17ad7af8e3
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-03-01 19:23:57 +0100

    Note version imprimable et pdf

---
+++ src/Controller/Admin/EtatsController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller\Admin;
+
+// namespace App\Controller;
+
+
+use App\Constantes\Constante;
+use App\Entity\Entreprise;
+use App\Entity\Utilisateur;
+use App\Services\ServiceTaxes;
+use App\Services\ServiceMonnaies;
+use App\Repository\NoteRepository;
+use App\Repository\InviteRepository;
+use App\Repository\EntrepriseRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+#[Route("/admin/etats", name: 'admin.etats.')]
+#[IsGranted('ROLE_USER')]
+class EtatsController extends AbstractController
+{
+    public function __construct(
+        private MailerInterface $mailer,
+        private EntrepriseRepository $entrepriseRepository,
+        private InviteRepository $inviteRepository,
+        private NoteRepository $noteRepository,
+        private TranslatorInterface $translator,
+        private EntityManagerInterface $manager,
+        private Constante $constante,
+        private ServiceMonnaies $serviceMonnaies,
+        private ServiceTaxes $serviceTaxes,
+    ) {}
+
+
+    #[Route(
+        '/imprimerNote/{idEntreprise}/{idNote}/{currentURL}',
+        name: 'imprimer_note',
+        requirements: [
+            'idEntreprise' => Requirement::DIGITS,
+            'idNote' => Requirement::DIGITS,
+            'currentURL' => Requirement::CATCH_ALL
+        ]
+    )]
+    public function imprimerNote($currentURL, $idEntreprise, $idNote, Request $request): Response
+    {
+        /** @var Utilisateur $utilisateur */
+        $utilisateur = $this->getUser();
+
+        /** @var Entreprise $entreprise */
+        $entreprise = $this->entrepriseRepository->find($idEntreprise);
+
+        /** @var Note $note */
+        $note = $this->noteRepository->find($idNote);
+
+        if ($note != null) {
+            # code...
+            // dd("Ici", $note, $currentURL);
+
+            return $this->render('admin/etats/note/index.html.twig', [
+                'controller_name' => 'EtatsController',
+                'entreprise' => $entreprise,
+                'utilisateur' => $utilisateur,
+                'note' => $note,
+                'constante' => $this->constante,
+                'serviceMonnaie' => $this->serviceMonnaies,
+                'serviceTaxe' => $this->serviceTaxes,
+                'date' => new DateTimeImmutable("now"),
+            ]);
+        } else {
+            $this->addFlash("danger", "Désolé " . $utilisateur->getNom() . ", la note est introuvable dans la base de données.");
+            return $this->redirect($currentURL);
+        }
+    }
+}
