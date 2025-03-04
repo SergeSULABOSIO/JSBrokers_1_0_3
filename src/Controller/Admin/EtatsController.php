ec233ca05b90c168f7940309ac58a33acd5a7dad
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-03-04 15:11:04 +0100

    Bordereau version imprimable et pdf

--- src/Controller/Admin/EtatsController.php
+++ src/Controller/Admin/EtatsController.php
@@ -136,6 +136,22 @@ class EtatsController extends AbstractController
         $note = $this->noteRepository->find($idNote);
 
         if ($note != null) {
+
+
+            // return $this->render(
+            //     'admin/etats/note/bordereau.html.twig',
+            //     [
+            //         'entreprise' => $entreprise,
+            //         'utilisateur' => $utilisateur,
+            //         'note' => $note,
+            //         'constante' => $this->constante,
+            //         'serviceMonnaie' => $this->serviceMonnaies,
+            //         'serviceTaxe' => $this->serviceTaxes,
+            //         'date' => new DateTimeImmutable("now"),
+            //     ]
+            // );
+
+
             $html = $this->renderView(
                 'admin/etats/note/bordereau.html.twig',
                 [
