d17b4b571dc87671f74e553129dfd50ac0f63064
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-03-12 09:30:56 +0100

    Note version imprimable et pdf - Avec TCPDF

--- src/Constantes/MyCustomTCPDF.php
+++ src/Constantes/MyCustomTCPDF.php
@@ -16,15 +16,15 @@ class MyCustomTCPDF extends TCPDF
     }
 
     // //Page header
-    // public function Header() {
-    //     // Logo
-    //     $image_file = K_PATH_IMAGES.'logo_example.jpg';
-    //     $this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
-    //     // Set font
-    //     $this->SetFont('helvetica', 'B', 20);
-    //     // Title
-    //     $this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
-    // }
+    public function Header() {
+        // Logo
+        $image_file = K_PATH_IMAGES . 'logo_example.jpg';
+        $this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        // Set font
+        $this->SetFont('helvetica', 'B', 10);
+        // Title
+        $this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+    }
 
     // Page footer
     public function Footer()
@@ -35,7 +35,7 @@ class MyCustomTCPDF extends TCPDF
         $nbPage = $this->getAliasNbPages();
         
         $this->SetFont('Times', 'N', 8);
-        
+
         $ligne1 = '<div style="font-weight: bold;text-align:center;">' . $this->entreprise->getNom() . '</div>';
         
         $codeMonnaieLocale = "";
