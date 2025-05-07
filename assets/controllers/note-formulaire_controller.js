73630411cdad37a4554d1f635eee27f6c0b19c5e
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-05-07 16:56:08 +0100

    Les roles

--- assets/controllers/note-formulaire_controller.js
+++ assets/controllers/note-formulaire_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static targets = ['nom', 'display'];
+
     connect() {
         console.log('Le contrôleur note-formulaire est connecté !');
     }
@@ -10,29 +12,39 @@ export default class extends Controller {
 
         console.log('Le bouton Précédent a été cliqué !');
         console.log(event.target);
-        
+
         event.target.disabled = true;
-        var OldLabel = event.target.innerText;
-        event.target.innerText = "PATIENTEZ SVP...";
-        
+        // var OldLabel = event.target.innerText;
+        // event.target.innerText = "PATIENTEZ SVP...";
+
+        this.displayTarget.textContent = "Enregistrement de " + this.nomTarget.value + " en cours...";
+        this.displayTarget.style.display = 'block';
+
         // Ici, vous pouvez ajouter votre logique AJAX, de validation, etc.
         const formData = new FormData(this.element); // 'this.element' fait référence à l'élément <form>
         fetch(this.element.action, {
             method: this.element.method,
             body: formData,
         })
-        .then(response => response.text()) //.json()
-        .then(data => {
-            event.target.disabled = false;
-            event.target.innerText = OldLabel;
-            console.log('Réponse du serveur :', data);
-            // Traitez la réponse ici
-        })
-        .catch(errorMessage => {
-            event.target.disabled = false;
-            event.target.innerText = OldLabel;
-            console.error("Réponse d'erreur du serveur :", errorMessage);
-            // Traitez la réponse ici
-        });
+            .then(response => response.text()) //.json()
+            .then(data => {
+                event.target.disabled = false;
+                
+                this.displayTarget.textContent = "Infos.";
+                this.displayTarget.style.display = 'none';
+
+                console.log('Réponse du serveur :', data);
+                // Traitez la réponse ici
+            })
+            .catch(errorMessage => {
+                event.target.disabled = false;
+                // event.target.innerText = OldLabel;
+
+                this.displayTarget.textContent = "Désolé, une erreur s'est produite, merci de vérifier vos données ou votre connexion Internet.";
+                this.displayTarget.style.display = 'none';
+
+                console.error("Réponse d'erreur du serveur :", errorMessage);
+                // Traitez la réponse ici
+            });
     }
 }
