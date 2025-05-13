f1fc757245a214f09ac8fd018093b7185f682773
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-05-13 12:51:43 +0100

    Les roles

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,18 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+/**
+ * @param {string} url 
+ * @param {htmlElement} elementHtml 
+ * @param {string} texteAccompagnement 
+ */
+export function defineIcone(url, elementHtml, texteAccompagnement) {
+    var iconeData = this.getIconeLocale(url);
+    if (iconeData != null) {
+        this.setIcone(iconeData, elementHtml, texteAccompagnement);
+    } else {
+        var data = url.split('/'); // var url = '/admin/entreprise/geticon/1/add/20';
+        this.downloadIcone(elementHtml, " " + texteAccompagnement, data[4], data[5], data[6]);
+    }
+}
