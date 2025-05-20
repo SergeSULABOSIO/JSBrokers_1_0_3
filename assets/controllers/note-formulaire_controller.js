0bea3ff6daa2f2fc91a7810ed8a7265d93b9dbf2
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-05-20 11:30:44 +0100

    Les roles

--- assets/controllers/note-formulaire_controller.js
+++ assets/controllers/note-formulaire_controller.js
@@ -192,7 +192,7 @@ export default class extends Controller {
                 // Traitez la réponse ici
                 if (this.isSaved == true) {
                     this.btArticlesTarget.style.display = 'inline';
-                    this.displayTarget.textContent = "Cliquez sur le bouton 'AJOUTER LES ARTICLES [...]' afin d'aller ajouter les articles dans la note.";
+                    this.displayTarget.textContent = "Cliquez sur le bouton 'AJOUTER LES ARTICLES' si vous voulez ajouter les articles dans cette note.";
 
                     //actualisation des autres composant du formulaire ainsi que du panier
                     const tabData = data.split("__1986__");
