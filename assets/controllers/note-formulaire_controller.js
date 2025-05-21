ba0a8bef39acbb4aa069fd22d06debc711298c6f
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-05-21 16:41:03 +0100

    Les roles

--- assets/controllers/note-formulaire_controller.js
+++ assets/controllers/note-formulaire_controller.js
@@ -49,6 +49,7 @@ export default class extends Controller {
             this.nbarticlesValue,
             this.nbpaiementsValue,
         );
+        this.displayTarget.textContent = "Prêt.";
     }
 
 
@@ -193,7 +194,7 @@ export default class extends Controller {
                 if (this.isSaved == true) {
                     this.btArticlesTarget.style.display = 'inline';
                     this.displayTarget.textContent = "Cliquez sur le bouton 'AJOUTER LES ARTICLES' si vous voulez ajouter les articles dans cette note.";
-                    
+
                     //actualisation des autres composant du formulaire ainsi que du panier
                     const tabData = data.split("__1986__");
                     
