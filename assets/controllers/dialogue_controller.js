1d1f908ba0e08618ff575c254feb323811900736
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-06-04 17:19:10 +0100

    Les roles

--- assets/controllers/dialogue_controller.js
+++ assets/controllers/dialogue_controller.js
@@ -1,18 +1,30 @@
 // assets/controllers/dialogue_controller.js
 import { Controller } from '@hotwired/stimulus';
+// Assurez-vous que Bootstrap est bien importé ici
+import * as bootstrap from 'bootstrap'; // ou import { Modal } from 'bootstrap'; si vous voulez seulement Modal
+
 
 export default class extends Controller {
-  static targets = ['boite'];
+    static targets = ['boite'];
 
-  connect() {
-    this.boite = new bootstrap.Modal(this.boiteTarget);
-  }
+    connect() {
+        // Vérifiez que this.boiteTarget existe bien et est un élément DOM
+        if (this.boiteTarget) {
+            this.boite = new bootstrap.Modal(this.boiteTarget);
+        } else {
+            console.error("Modal target not found!");
+        }
+    }
 
-  open() {
-    this.boite.show();
-  }
+    open() {
+        if (this.boite) {
+            this.boite.show();
+        }
+    }
 
-  close() {
-    this.boite.hide();
-  }
+    close() {
+        if (this.boite) {
+            this.boite.hide();
+        }
+    }
 }
