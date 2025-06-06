25e1aee3a51fda00e07802b58b5db6cc9bf6827f
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-06-06 13:27:05 +0100

    Les roles

--- src/Entity/NotificationSinistre.php
+++ src/Entity/NotificationSinistre.php
@@ -15,7 +15,7 @@ class NotificationSinistre
     #[ORM\Column]
     private ?int $id = null;
     
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $referencePolice = null;
 
     #[ORM\Column(length: 255, nullable: true)]
