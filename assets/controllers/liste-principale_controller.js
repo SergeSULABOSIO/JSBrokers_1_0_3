45d3f3e430b2d52b7da7dbcc987bcc3a22bd0ead
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-06-09 10:31:19 +0100

    Les roles

---
+++ assets/controllers/liste-principale_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = [
+        'display',  //Champ d'affichage d'informations
+        'donnees',     //Liste conténant des élements
+    ];
+    static values = {
+        objet: Number,
+        identreprise: Number,
+        idutilisateur: Number,
+        nbelements: Number,
+        rubrique: String,
+    };
+
+
+    connect() {
+        console.log("Liste " + this.rubriqueValue);
+        this.updateMessage("Prêt: " + this.nbelementsValue + " élement(s).");
+    }
+
+    /**
+     * @param {Event} event 
+    */
+    selectionner(event) {
+        const cible = event.currentTarget;
+        this.objetValue = cible.dataset.itemObjet;
+        console.log("Liste : Element selectionné: ", event.currentTarget, this.objetValue);
+    }
+
+
+    /**
+     * 
+     * @param {string} newMessage 
+     */
+    updateMessage(newMessage) {
+        this.displayTarget.innerHTML = newMessage;
+    }
+}
