7668a0272b403f4c0260701c0b35a81f223d838c
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-08-14 11:26:18 +0100

    Construction dynamique du tableau selon la entityFormCanvas

--- src/Constantes/Constante.php
+++ src/Constantes/Constante.php
@@ -6266,41 +6266,42 @@ class Constante
                     "titre_modification" => "Modification de la Notification #%id%", // %id% sera remplacé par l'ID
                     "endpoint_submit_url" => "/admin/notificationsinistre/api/submit",
                     "endpoint_form_url" => "/admin/notificationsinistre/api/get-form",
+                    
+                    // C'est ici que l'on décrit la mise en page souhaitée
+                    "form_layout" => [
+                        // Ligne 1 : 1 colonne pour l'assuré
+                        ["colonnes" => [
+                            ["champs" => ["assure"]],
+                            ["champs" => ["assureur"]]
+                        ]],
+                        ["colonnes" => [
+                            ["champs" => ["risque"]]
+                        ]],
+                        // Ligne 2 : 2 colonnes
+                        ["colonnes" => [
+                            ["champs" => ["referencePolice"]],
+                            ["champs" => ["referenceSinistre"]]
+                        ]],
+                        // Ligne 3 : 1 colonne large
+                        ["colonnes" => [
+                            ["champs" => ["descriptionDeFait"]]
+                        ]],
+                        // Ligne 4 : 2 colonnes
+                        ["colonnes" => [
+                            ["champs" => ["occuredAt"]],
+                            ["champs" => ["notifiedAt"]]
+                        ]],
+                        ["colonnes" => [
+                            ["champs" => ["descriptionVictimes"]],
+                            ["champs" => ["lieu"]]
+                        ]],
+                        ["colonnes" => [
+                            ["champs" => ["dommage"]],
+                            ["champs" => ["evaluationChiffree"]]
+                        ]],
+                        // ... Ajoutez d'autres lignes et champs selon vos besoins
+                    ],
                 ],
-                // C'est ici que l'on décrit la mise en page souhaitée
-                "form_layout" => [
-                    // Ligne 1 : 1 colonne pour l'assuré
-                    ["colonnes" => [
-                        ["champs" => ["assure"]],
-                        ["champs" => ["assureur"]]
-                    ]],
-                    ["colonnes" => [
-                        ["champs" => ["risque"]]
-                    ]],
-                    // Ligne 2 : 2 colonnes
-                    ["colonnes" => [
-                        ["champs" => ["referencePolice"]],
-                        ["champs" => ["referenceSinistre"]]
-                    ]],
-                    // Ligne 3 : 1 colonne large
-                    ["colonnes" => [
-                        ["champs" => ["descriptionDeFait"]]
-                    ]],
-                    // Ligne 4 : 2 colonnes
-                    ["colonnes" => [
-                        ["champs" => ["occuredAt"]],
-                        ["champs" => ["notifiedAt"]]
-                    ]],
-                    ["colonnes" => [
-                        ["champs" => ["descriptionVictimes"]],
-                        ["champs" => ["lieu"]]
-                    ]],
-                    ["colonnes" => [
-                        ["champs" => ["dommage"]],
-                        ["champs" => ["evaluationChiffree"]]
-                    ]],
-                    // ... Ajoutez d'autres lignes et champs selon vos besoins
-                ]
             ];
         }
         return [];
