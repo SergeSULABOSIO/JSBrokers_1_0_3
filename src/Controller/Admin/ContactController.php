7f45d650da27e4b2be4bc798351fc4b4b75f2753
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-08-26 18:03:48 +0100

    Construction dynamique du tableau selon la entityFormCanvas - Champs de type Collection - recustion à l'infini

--- src/Controller/Admin/ContactController.php
+++ src/Controller/Admin/ContactController.php
@@ -6,13 +6,11 @@ use App\Entity\Contact;
 use App\Entity\Assureur;
 use App\Form\ContactType;
 use App\Entity\Entreprise;
-use App\Form\AssureurType;
 use App\Constantes\Constante;
 use App\Constantes\MenuActivator;
 use App\Entity\NotificationSinistre;
 use App\Repository\InviteRepository;
 use App\Repository\ContactRepository;
-use App\Repository\AssureurRepository;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -58,93 +56,7 @@ class ContactController extends AbstractController
             'activator' => $this->activator,
         ]);
     }
-
-
-    #[Route('/create/{idEntreprise}', name: 'create')]
-    public function create($idEntreprise, Request $request)
-    {
-        /** @var Entreprise $entreprise */
-        $entreprise = $this->entrepriseRepository->find($idEntreprise);
-
-        /** @var Utilisateur $user */
-        $user = $this->getUser();
-
-        /** @var Contact $contact */
-        $contact = new Contact();
-        //Paramètres par défaut
-        // $contact->setEntreprise($entreprise);
-
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->manager->persist($contact);
-            $this->manager->flush();
-
-            return new Response("Ok");
-        }
-        return $this->render('admin/contact/create.html.twig', [
-            'pageName' => $this->translator->trans("contact_page_name_new"),
-            'utilisateur' => $user,
-            'entreprise' => $entreprise,
-            'contact' => $contact,
-            'activator' => $this->activator,
-            'form' => $form,
-        ]);
-    }
-
-
-    #[Route('/edit/{idEntreprise}/{idContact}', name: 'edit', requirements: ['idEntreprise' => Requirement::DIGITS], methods: ['GET', 'POST'])]
-    public function edit($idEntreprise, $idContact, Request $request)
-    {
-        /** @var Entreprise $entreprise */
-        $entreprise = $this->entrepriseRepository->find($idEntreprise);
-
-        /** @var Utilisateur $user */
-        $user = $this->getUser();
-
-        /** @var Contact $contact */
-        $contact = $this->contactRepository->find($idContact);
-
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->manager->persist($contact); //On peut ignorer cette instruction car la fonction flush suffit.
-            $this->manager->flush();
-
-            return new Response("Ok");
-        }
-        return $this->render('admin/contact/edit.html.twig', [
-            'pageName' => $this->translator->trans("contact_page_name_update", [
-                ":contact" => $contact->getNom(),
-            ]),
-            'utilisateur' => $user,
-            'contact' => $contact,
-            'entreprise' => $entreprise,
-            'activator' => $this->activator,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/remove/{idEntreprise}/{idContact}', name: 'remove', requirements: ['idContact' => Requirement::DIGITS, 'idEntreprise' => Requirement::DIGITS], methods: ['DELETE'])]
-    public function remove($idEntreprise, $idContact, Request $request)
-    {
-        /** @var Contact $contact */
-        $contact = $this->contactRepository->find($idContact);
-
-        $message = $this->translator->trans("contact_deletion_ok", [
-            ":contact" => $contact->getNom(),
-        ]);;
-
-        $this->manager->remove($contact);
-        $this->manager->flush();
-
-        $this->addFlash("success", $message);
-        return $this->redirectToRoute("admin.contact.index", [
-            'idEntreprise' => $idEntreprise,
-        ]);
-    }
+    
 
     /**
      * Fournit le formulaire HTML pour un contact (nouveau ou existant).
