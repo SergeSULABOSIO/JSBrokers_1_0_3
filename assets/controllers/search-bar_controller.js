2b5feb76b9eeb4155a1913c5aed844450b324394
Serge SULA BOSIO <sulabosiog@gmail.com>
2025-11-08 21:09:34 +0100

    Refonte de la page principale - ajout du system de menu à onglets dynamique

--- assets/controllers/search-bar_controller.js
+++ assets/controllers/search-bar_controller.js
@@ -137,7 +137,7 @@ export default class extends BaseController {
         const backdrops = document.querySelectorAll('.modal-backdrop.show');
 
         // S'il y a plus d'une modale affichée, un ajustement est nécessaire pour la superposition.
-        if (modals.length > 1) {
+        // if (modals.length > 1) {
             let maxZIndex = 0;
             // 1. On cherche le z-index le plus élevé parmi TOUTES les autres modales.
             modals.forEach(modal => {
@@ -155,9 +155,9 @@ export default class extends BaseController {
 
             // 3. On définit le z-index de notre backdrop pour être juste au-dessus du maximum trouvé,
             //    et celui de notre modale pour être au-dessus de son propre backdrop.
-            myBackdrop.style.zIndex = maxZIndex + 1;
+            myBackdrop.style.zIndex = maxZIndex + 100;
             myModal.style.zIndex = maxZIndex + 2;
-        }
+        // }
     }
 
     /**
