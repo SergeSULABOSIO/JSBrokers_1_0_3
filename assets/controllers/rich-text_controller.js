92bf906016708c95a13e118cae95275784e8f78c
Serge SULA BOSIO <sulabosiog@gmail.com>
2026-02-25 02:58:06 +0100

    Modernisation de la collection Revenus dans le formulaire d'édition de la cotation

---
+++ assets/controllers/rich-text_controller.js
@@ -0,0 +1,68 @@
+import { Controller } from '@hotwired/stimulus';
+import Quill from 'quill';
+// Assurez-vous que le CSS de Quill est importé. Si vous utilisez Webpack Encore, cela fonctionne généralement :
+import 'quill/dist/quill.snow.css'; 
+
+/**
+ * @class RichTextController
+ * @description Transforme un textarea standard en éditeur de texte riche avec Quill.
+ */
+export default class extends Controller {
+    connect() {
+        // Configuration de la barre d'outils avec les options demandées (mise en forme, couleur, etc.)
+        const toolbarOptions = [
+            ['bold', 'italic', 'underline', 'strike'],        // Boutons de style
+            [{ 'color': [] }, { 'background': [] }],          // Couleur du texte et surlignage
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],     // Listes
+            [{ 'align': [] }],                                // Alignement
+            [{ 'header': [1, 2, 3, false] }],                 // Titres
+            ['clean']                                         // Bouton pour effacer le formatage
+        ];
+
+        // Création du conteneur visuel pour l'éditeur
+        const editorContainer = document.createElement('div');
+        // On définit une hauteur par défaut confortable (similaire à votre classe .editeur-riche)
+        editorContainer.style.height = '200px'; 
+        editorContainer.style.backgroundColor = '#fff'; // Fond blanc pour l'éditeur
+        
+        // On insère l'éditeur juste avant le textarea
+        this.element.parentNode.insertBefore(editorContainer, this.element);
+        
+        // On cache le textarea original (qui servira de stockage caché)
+        this.element.style.display = 'none';
+
+        // Initialisation de Quill
+        this.quill = new Quill(editorContainer, {
+            modules: {
+                toolbar: toolbarOptions
+            },
+            theme: 'snow', // Thème standard propre
+            placeholder: this.element.getAttribute('placeholder') || 'Saisissez votre texte ici...'
+        });
+
+        // On charge le contenu initial du textarea dans l'éditeur
+        this.quill.root.innerHTML = this.element.value;
+
+        // À chaque modification dans l'éditeur, on met à jour le textarea caché
+        this.quill.on('text-change', () => {
+            this.element.value = this.quill.root.innerHTML;
+            // On déclenche l'événement 'change' manuellement pour que d'autres scripts le détectent
+            this.element.dispatchEvent(new Event('change'));
+        });
+    }
+
+    disconnect() {
+        // Nettoyage si le contrôleur est retiré
+        if (this.quill) {
+            const container = this.quill.container.parentNode; // Le wrapper .ql-container
+            const toolbar = container.previousSibling; // La barre d'outils .ql-toolbar
+            
+            if (toolbar && toolbar.classList.contains('ql-toolbar')) {
+                toolbar.remove();
+            }
+            container.remove();
+            
+            this.element.style.display = 'block'; // Réafficher le textarea
+        }
+    }
+}
