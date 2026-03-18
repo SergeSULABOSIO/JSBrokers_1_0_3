f7d84f8e5ffde95de032942db83aacb3a38797e7
Serge SULA BOSIO <sulabosiog@gmail.com>
2026-03-18 09:16:05 +0100

    Modernisation des entités en appliquant les canvas refcatorisés

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
